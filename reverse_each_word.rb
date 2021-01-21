d4277a2563a2340c37c54ce56888811c86a876d4
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-21 21:01:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(string)
   my_array = string.split()
-  my_array.collect {|element| element.reverse}
-  my_array.join
+  my_array.collect {|element| element.reverse}.join("")
 
 end
